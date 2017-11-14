d0fd851e70919db9ec97b68a3425abe0a27865f9
rousakis <rousakis@costner.ics.forth.gr>
2017-11-14 16:10:55 +0200

    Changes -

--- src/main/java/forth/ics/isl/controller/UserProfileController.java
+++ src/main/java/forth/ics/isl/controller/UserProfileController.java
@@ -1,20 +1,14 @@
 package forth.ics.isl.controller;
 
-import static forth.ics.isl.service.QueryGenService.geoEntityQuery;
-
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.ws.rs.core.Response;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Controller;
@@ -25,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import forth.ics.isl.service.DBService;
-import forth.ics.isl.triplestore.RestClient;
+import java.util.LinkedHashMap;
 
 /**
  * The back-end controller for the User Profile Service activities
@@ -36,56 +30,56 @@ import forth.ics.isl.triplestore.RestClient;
 @Controller
 public class UserProfileController {
 
-	@Autowired
-	private DBService dbService;
+    @Autowired
+    private DBService dbService;
 
     @PostConstruct
     public void init() throws IOException, SQLException {
         dbService = new DBService();
     }
-    
+
     /**
      * Saving the queryBuilder JSON model into the database for specific user
-     * 
-     * @param authorizationToken 	A string holding the authorization token
-     * @param username 				The username of the user
-     * @param username 				The title of the user
-     * @param username 				The description of the user
-     * @param queryModel 			The query JSON model used by the QueryBuilder
+     *
+     * @param authorizationToken A string holding the authorization token
+     * @param username The username of the user
+     * @param username The title of the user
+     * @param username The description of the user
+     * @param queryModel The query JSON model used by the QueryBuilder
      */
     @RequestMapping(value = "/save_into_favorites", method = RequestMethod.POST, produces = {"application/json"})
     public @ResponseBody
     JSONObject saveIntoFavorites(@RequestHeader(value = "Authorization") String authorizationToken, @RequestBody JSONObject requestParams) throws IOException, ParseException {
-    	System.out.println("fromSearch:" + requestParams.get("username"));
-    	System.out.println("fromSearch:" + requestParams.get("queryModel"));
-    	System.out.println("fromSearch:" + requestParams.get("title"));
-    	System.out.println("fromSearch:" + requestParams.get("description"));
-        
-    	JSONObject statusObject = new JSONObject();
-    	statusObject = dbService.saveIntoFavorites(
-    			requestParams.get("username").toString(), 
-	    		requestParams.get("title").toString(), 
-	    		requestParams.get("description").toString(), 
-	    		requestParams.get("queryModel").toString()
-    	);
-    	
+        System.out.println("fromSearch:" + requestParams.get("username"));
+        System.out.println("fromSearch:" + requestParams.get("queryModel"));
+        System.out.println("fromSearch:" + requestParams.get("title"));
+        System.out.println("fromSearch:" + requestParams.get("description"));
+
+        JSONObject statusObject = new JSONObject();
+        JSONObject queryModel = new JSONObject((LinkedHashMap) requestParams.get("queryModel"));
+        statusObject = dbService.saveIntoFavorites(
+                requestParams.get("username").toString(),
+                requestParams.get("title").toString(),
+                requestParams.get("description").toString(),
+                queryModel.toString()
+        );
+
         return statusObject;
     }
-    
-    /** 
+
+    /**
      * Retrieving user's list of query models from favorites
-     * 
-     * @param authorizationToken 	A string holding the authorization token
-     * @param username 				The username of the user
+     *
+     * @param authorizationToken A string holding the authorization token
+     * @param username The username of the user
      */
     @RequestMapping(value = "/retrieve_favorite_query_models_by_user", method = RequestMethod.POST, produces = {"application/json"})
     public @ResponseBody
     JSONArray loadFavoriteQueryModel(@RequestHeader(value = "Authorization") String authorizationToken, @RequestBody JSONObject requestParams) throws IOException, ParseException {
-    	System.out.println("fromSearch:" + requestParams.get("username"));
-        
-    	JSONArray queryModelsArray = new JSONArray();
+        System.out.println("fromSearch:" + requestParams.get("username"));
+
+        JSONArray queryModelsArray = new JSONArray();
         return queryModelsArray;
     }
-    
-    
+
 }
